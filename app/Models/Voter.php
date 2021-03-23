606dbd971e5af4be47186062861b47bff1fa0388
Ramdani Surya M <a2.1800105@mhs.stmik-sumedang.ac.id>
2021-03-23 22:44:15 +0700

    prepare for voters authentication

--- app/Models/Voter.php
+++ app/Models/Voter.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class Voter extends Model
+class Voter extends Authenticatable
 {
     use HasFactory;
 }
