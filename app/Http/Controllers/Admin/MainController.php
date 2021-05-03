907880a836862001be21bf19cbecf4f3fa97913f
Ramdani Surya <ramdanisurya721@gmail.com>
2021-05-03 13:07:15 +0700

    membuat chart dan widget menjadi realtime

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -4,65 +4,63 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Candidate;
-use App\Models\Election;
-use App\Models\Voter;
-use App\Models\Voting;
-use App\Models\User;
-use DB;
-use Carbon\Carbon;
 
 
 class MainController extends Controller
 {
-    public function dashboard(Election $election)
+    public function dashboard()
     {
-        $jumlah_kandidat = Candidate::All();
-        $candidate = getActiveElection() ? count(getActiveElection()->candidates) : 0;
-        $candidates = Candidate::all();
-        $not_voted = votersPercentage($election, 0);
-        $candidateArray = [];
+
+        if (getActiveElection()) {
+            $data = $this->getVotingData();
+        } else {
+            $data['candidates'] = [];
+            $data['candidateVotings'] = [];
+        }
+
+        return view('admin.dashboard.data', $data);
+    }
+
+    public function dashboardApi()
+    {
+        // set data buat widget
+        $data = [
+            'total_voter' => count(getActiveElection()->voters),
+            'has_voted'   => [
+                'total'      => count(getActiveElection()->votedVoters),
+                'percentage' => votersPercentage(getActiveElection()),
+            ],
+            'unvoted' => [
+                'total'      => count(getActiveElection()->unvotedVoters),
+                'percentage' => votersPercentage(getActiveElection(), 0),
+            ],
+            'total_candidate' => count(getActiveElection()->candidates),
+        ];
+
+        $data['votings'] = $this->getVotingData()['candidateVotings'];
+
+        return response()->json($data);
+    }
+
+    // set data buat chart
+    private function getVotingData()
+    {
+        $candidates = [];
         $candidateVotings = [];
 
-        foreach ($candidates as $dcandidate) {
-            $candidateArray[] = "$dcandidate->chairman_name - $dcandidate->vice_chairman_name";
-            $candidateVotings[] = count($dcandidate->votings);
+        if (getActiveElection()) {
+            foreach (getActiveElection()->candidates as $candidate) {
+                $candidates[] = "$candidate->chairman_name - $candidate->vice_chairman_name";
+                $candidateVotings[] = count($candidate->votings);
+            }
+
+            $candidates[] = 'Belum Memilih';
+            $candidateVotings[] = count(getActiveElection()->unvotedVoters);
         }
-        $candidateArray[] = 'Belum Memilih';
-        $candidateVotings[] = $not_voted;
 
+        $votingData['candidates'] = $candidates;
+        $votingData['candidateVotings'] = $candidateVotings;
 
-        $jumlah_voter = Voter::All();
-        $voter = $jumlah_voter->count();
-
-        $voted = votersPercentage($election, 1);
-
-        $sudah_memilih = Voter::where('voted', '1')->get();
-
-        $kandidat1 = Voting::where('candidate_id', '17')->get();
-        $jumlah = $kandidat1->count();
-
-        $user = User::where('role','admin')->get();
-        $jumlahAdmin = $user->count();
-        $chartjs = app()->chartjs
-         ->name('barChartTest')
-         ->type('bar')
-         ->size(['width' => 400, 'height' => 200])
-         ->labels($candidateArray)
-         ->datasets([
-             [
-                 "label" => "Suara",
-                 'backgroundColor' => ['#fff', 'rgba(54, 162, 235, 0.2)', '#acacac'],
-                 'data' => $candidateVotings
-             ],
-
-
-
-         ])
-         ->options([]);
-
-
-
-        return view('admin.dashboard.data', compact('candidate','voter','voted','not_voted','chartjs'));
+        return $votingData;
     }
 }
--- resources/views/admin/dashboard/data.blade.php
+++ resources/views/admin/dashboard/data.blade.php
@@ -3,37 +3,35 @@
 @section('subtitle')
 Dashboard
 @endsection
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 @section('content')
 <!-- start row -->
-
 <div class="row text-center">
     <div class="col-sm-6 col-xl-3">
         <div class="card-box widget-flat border-primary bg-primary text-white">
             <i class="fas fa-poll"></i>
-            <h3 class="text-white">{{ $voter }}</h3>
+            <h3 class="text-white" id="total-voter"></h3>
             <p class="text-uppercase font-13 mb-2 font-weight-bold">Jumlah Pemilih</p>
         </div>
     </div>
     <div class="col-sm-6 col-xl-3">
         <div class="card-box bg-blue widget-flat border-blue text-white">
             <i class="fas fa-vote-yea"></i>
-            <h3 class="text-white">{{ $voted }}</h3>
+            <h3 class="text-white" id="has-voted"></h3>
             <p class="text-uppercase font-13 mb-2 font-weight-bold">Sudah Memilih</p>
         </div>
     </div>
     <div class="col-sm-6 col-xl-3">
         <div class="card-box widget-flat border-success bg-success text-white">
             <i class="fab fa-snapchat-ghost"></i>
-            <h3 class="text-white">{{ $not_voted }}</h3>
+            <h3 class="text-white" id="unvoted"></h3>
             <p class="text-uppercase font-13 mb-2 font-weight-bold">Belum Memilih</p>
         </div>
     </div>
     <div class="col-sm-6 col-xl-3">
         <div class="card-box bg-warning widget-flat border-danger text-white">
             <i class="fas fa-poll-h"></i>
-            <h3 class="text-white">{{ $candidate }}</h3>
+            <h3 class="text-white" id="total-candidate"></h3>
             <p class="text-uppercase font-13 mb-2 font-weight-bold">Jumlah Kandidat</p>
         </div>
     </div>
@@ -42,15 +40,13 @@ Dashboard
 
 <div class="card-box d-flex justify-content-center">
     <div style="width:60%;">
-        {!! $chartjs->render() !!}
+        <canvas id="myChart" data-candidates="{{ implode(',', $candidates) }}"
+            data-votings="{{ implode(',', $candidateVotings) }}"></canvas>
     </div>
 </div>
-
-
 @endsection
 
 @section('js')
-
 <!-- Flot chart -->
 <script src="{{ asset('highdmin/libs/flot-charts/jquery.flot.js') }}"></script>
 <script src="{{ asset('highdmin/libs/flot-charts/jquery.flot.time.js') }}"></script>
@@ -68,7 +64,69 @@ Dashboard
 <script src="{{ asset('highdmin/js/pages/dashboard.init.js') }}"></script>
 
 <!-- ChartJs -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.0/chart.min.js"
-    integrity="sha512-RGbSeD/jDcZBWNsI1VCvdjcDULuSfWTtIva2ek5FtteXeSjLfXac4kqkDRHVGf1TwsXCAqPTF7/EYITD0/CTqw=="
-    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const chartArea = $('#myChart')
+
+    let data = {
+        labels: chartArea.data('candidates').split(','),
+        datasets: [{
+            label: 'Perolehan Suara',
+            backgroundColor: [
+                'rgba(237, 174, 73, 1)',
+                'rgba(209, 73, 91, 1)',
+                'rgba(0, 121, 140, 1)',
+                'rgba(48, 99, 142, 1)'
+            ],
+            borderColor: ['#EDAE49', '#D1495B', '#00798C', '#30638E'],
+            data: chartArea.data('votings').split(',')
+        }]
+    };
+
+    const config = {
+        type: 'bar',
+        data
+    };
+
+    let myChart = new Chart(
+        chartArea,
+        config
+    );
+
+    myChart.options.animation = false;
+
+    function addData(chart, data) {
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data = data;
+        });
+
+        chart.update();
+    }
+
+    function removeData(chart) {
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data = null;
+        });
+
+        chart.update();
+    }
+
+
+    setInterval(() => {
+        $.getJSON('{{ route('dashboard_api') }}', null,
+            function (data, textStatus, jqXHR) {
+                $('#total-voter').text(data.total_voter);
+                $('#has-voted').text(`${data.has_voted.total} (${data.has_voted.percentage})`);
+                $('#unvoted').text(`${data.unvoted.total} (${data.unvoted.percentage})`);
+                $('#total-candidate').text(data.total_candidate);
+
+                removeData(myChart)
+                addData(myChart, data.votings)
+            }
+        );
+
+    }, 1000);
+
+</script>
 @endsection
