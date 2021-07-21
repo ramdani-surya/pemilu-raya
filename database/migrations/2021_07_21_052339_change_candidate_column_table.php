b88bf426ec618eb7a583a615b117df5cfd81b846
Ikbal <ikbal.malik26@gmail.com>
2021-07-21 12:32:19 +0700

    updates

--- database/migrations/2021_07_21_052339_change_candidate_column_table.php
+++ database/migrations/2021_07_21_052339_change_candidate_column_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateUserTableColumn extends Migration
+class ChangeCandidateColumnTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class UpdateUserTableColumn extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'panitia', 'saksi'])->default('panitia')->after('password');
+        Schema::table('candidates', function (Blueprint $table) {
+            $table->text('program')->change();
         });
     }
 
