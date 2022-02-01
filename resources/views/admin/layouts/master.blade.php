36596e16597d5181b05d5df18c18f392325056ae
hippies67 <ikbal.malik26@gmail.com>
2022-02-01 14:13:06 +0700

    perubahan

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -41,7 +41,7 @@
         ***********************************-->
         <div class="nav-header">
             <a href="index.html" class="brand-logo">
-                <img class="logo-abbr" src="{{ asset('images/admin_component/tahu.png') }}" alt="">
+                <img class="logo-abbr" src="{{ asset('images/admin_component/logo_pemira.png') }}" alt="">
                 <img class="logo-compact" src="{{ asset('images/logo-text.png') }}" alt="">
                 <span class="brand-title" style="font-weight: bold; color: rgb(150, 155, 160);">Pemilu Raya</span>
             </a>
@@ -582,21 +582,17 @@
 						
                             <li class="nav-item dropdown header-profile">
                                 <a class="nav-link" href="javascript:void(0)" role="button" data-toggle="dropdown">
-                                    {{-- <img src="{{ asset('images/profile/17.jpg') }}" width="20" alt=""/> --}}
+                                    <img src="{{ asset('images/admin_component/user.png') }}" width="20" style="object-fit: cover;" alt=""/>
 									<div class="header-info">
 										<span class="text-black"><strong>{{ Auth::user()->username }}</strong></span>
-										<p class="fs-12 mb-0">{{ Auth::user()->role }}</p>
+										<p class="fs-12 mb-0">{{ ucwords(str_replace('_', ' ', Auth::user()->role)) }}</p>
 									</div>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right">
-                                    {{-- <a href="./app-profile.html" class="dropdown-item ai-icon">
+                                    <a href="{{ route('admin.setting') }}" class="dropdown-item ai-icon">
                                         <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                                         <span class="ml-2">Profile </span>
                                     </a>
-                                    <a href="./email-inbox.html" class="dropdown-item ai-icon">
-                                        <svg id="icon-inbox" xmlns="http://www.w3.org/2000/svg" class="text-success" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
-                                        <span class="ml-2">Inbox </span>
-                                    </a> --}}
                                     <a href="{{ route('admin.logout') }}" class="dropdown-item ai-icon">
                                         <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
                                         <span class="ml-2">Logout </span>
@@ -637,6 +633,11 @@
                     <li class="{{ request()->routeIs('faculties.index') ? 'mm-active' : '' }}"><a class="ai-icon" href="{{ route('faculties.index') }}" aria-expanded="false">
 							<i class="flaticon-381-home-2"></i>
 							<span class="nav-text">Fakultas</span>
+						</a>
+                    </li>
+                    <li class="{{ request()->routeIs('candidate-types.index') ? 'mm-active' : '' }}"><a class="ai-icon" href="{{ route('candidate-types.index') }}" aria-expanded="false">
+							<i class="flaticon-381-id-card"></i>
+							<span class="nav-text">Tipe Kandidat</span>
 						</a>
                     </li>
                     <li class="{{ request()->routeIs('candidates.index') ? 'mm-active' : ''}} 
