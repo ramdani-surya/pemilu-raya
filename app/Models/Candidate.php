81497540c464713114a7709af8a994335ede61f4
hippies67 <ikbal.malik26@gmail.com>
2022-02-01 19:27:22 +0700

    another changes

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -34,38 +34,46 @@ class Candidate extends Model
 
     public function ftiVotings()
     {
-        $fti = Faculty::where('slug', 'fakultas-teknik-informatika')->first();
-        return $this->hasMany(Voting::class)->where('faculty_id', $fti->id);
+        $fti = Faculty::where('slug', 'fakultas-teknik-informatika')->first() ?: 0;
+        $fti_id = $fti ? $fti->id :0 ;
+
+        return $this->hasMany(Voting::class)->where('faculty_id', $fti_id);
     }
 
     public function febVotings()
     {
-        $feb = Faculty::where('slug', 'fakultas-ekonomi-dan-bisnis')->first();
-        return $this->hasMany(Voting::class)->where('faculty_id', $feb->id);
+        $feb = Faculty::where('slug', 'fakultas-ekonomi-dan-bisnis')->first() ?: 0;
+        $feb_id = $feb ? $feb->id :0 ;
+
+        return $this->hasMany(Voting::class)->where('faculty_id', $feb_id);
     }
 
     public function fisipVotings()
     {
-        $fisip = Faculty::where('slug', 'fakultas-ilmu-sosial-dan-ilmu-politik')->first();
-        return $this->hasMany(Voting::class)->where('faculty_id', $fisip->id);
+        $fisip = Faculty::where('slug', 'fakultas-ilmu-sosial-dan-ilmu-politik')->first() ?: 0;
+        $fisip_id = $fisip ? $fisip->id :0 ;
+        return $this->hasMany(Voting::class)->where('faculty_id', $fisip_id);
     }
 
     public function fkipVotings()
     {
-        $fkip = Faculty::where('slug', 'fakultas-keguruan-dan-ilmu-pendidikan')->first();
-        return $this->hasMany(Voting::class)->where('faculty_id', $fkip->id);
+        $fkip = Faculty::where('slug', 'fakultas-keguruan-dan-ilmu-pendidikan')->first() ?: 0;
+        $fkip_id = $fkip ? $fkip->id :0 ;
+        return $this->hasMany(Voting::class)->where('faculty_id', $fkip_id);
     }
 
     public function fibVotings()
     {
-        $fib = Faculty::where('slug', 'fakultas-ilmu-budaya')->first();
-        return $this->hasMany(Voting::class)->where('faculty_id', $fib->id);
+        $fib = Faculty::where('slug', 'fakultas-ilmu-budaya')->first() ?: 0;
+        $fib_id = $fib ? $fib->id :0 ;
+        return $this->hasMany(Voting::class)->where('faculty_id', $fib_id);
     }
 
     public function fikVotings()
     {
-        $fik = Faculty::where('slug', 'fakultas-ilmu-kesehatan')->first();
-        return $this->hasMany(Voting::class)->where('faculty_id', $fik->id);
+        $fik = Faculty::where('slug', 'fakultas-ilmu-kesehatan')->first() ?: 0;
+        $fik_id = $fik ? $fik->id :0 ;
+        return $this->hasMany(Voting::class)->where('faculty_id', $fik_id);
     }
 
     public function faculties()
